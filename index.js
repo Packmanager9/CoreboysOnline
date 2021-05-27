addfd05823e53a179857a9fc929dd3b0edfee0e3
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2021-05-26 17:43:04 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const INDEX = '/index.html';
 
 const server = express()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .listen(PORT, () => //console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
 let boys = []
@@ -19,23 +19,23 @@ let games = []
 let game = []
 
 wss.on("connection", ws => {
-    console.log("new client")
-    console.log(wss.clients.size)
+    //console.log("new client")
+    //console.log(wss.clients.size)
     game.push(ws)
     ws.on("close", () => {
-        console.log("client left")
+        //console.log("client left")
         game.splice(game.indexOf(ws))
     })
     ws.on("message", data => {
         if (data >= 0) {
-            // console.log(data)
-            // console.log([game.indexOf(ws), game.length])
+            // //console.log(data)
+            // //console.log([game.indexOf(ws), game.length])
 
             let sjon = {
                 "index":`${game.indexOf(ws)}`,
                 "length":`${game.length}`
              }
-            // console.log(sjon)
+            // //console.log(sjon)
             ws.publicID = data
             ws.send(JSON.stringify(sjon))
         } else {
