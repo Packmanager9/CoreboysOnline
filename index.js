71c55f78c620e6922f30ff2e47fe88956693ac85
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2021-05-28 01:28:41 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -27,34 +27,34 @@ wss.on("connection", ws => {
         let deleter = ws.index
 
         let minarr = []
-        for(let t = 0;t<game.length;t++){
+        for (let t = 0; t < game.length; t++) {
             minarr.push(game[t].pair[1])
         }
-        if(Math.max(...minarr) == -1){
+        if (Math.max(...minarr) == -1) {
             ws.pair[1] = 0
-        }else{
-            if(!minarr.includes(7)){
+        } else {
+            if (!minarr.includes(7)) {
                 ws.pair[1] = 7
             }
-            if(!minarr.includes(6)){
+            if (!minarr.includes(6)) {
                 ws.pair[1] = 6
             }
-            if(!minarr.includes(5)){
+            if (!minarr.includes(5)) {
                 ws.pair[1] = 5
             }
-            if(!minarr.includes(4)){
+            if (!minarr.includes(4)) {
                 ws.pair[1] = 4
             }
-            if(!minarr.includes(3)){
+            if (!minarr.includes(3)) {
                 ws.pair[1] = 3
             }
-            if(!minarr.includes(2)){
+            if (!minarr.includes(2)) {
                 ws.pair[1] = 2
             }
-            if(!minarr.includes(1)){
+            if (!minarr.includes(1)) {
                 ws.pair[1] = 1
             }
-            if(!minarr.includes(0)){
+            if (!minarr.includes(0)) {
                 ws.pair[1] = 0
             }
         }
@@ -67,130 +67,143 @@ wss.on("connection", ws => {
             }
             // sjon.minarr = minarr
             let ids = []
-            for(let t = 0;t<game.length;t++){
-                if(t != game.indexOf(ws)){
+            for (let t = 0; t < game.length; t++) {
+                if (t != game.indexOf(ws)) {
                     // console.log(game[t].serverID)
                     ids.push(game[t].serverID)
                 }
             }
             sjon.playerIDs = ids
-            
+
             game[t].send(JSON.stringify(sjon))
         }
     })
     ws.on("message", data => {
-        if (data >= 0) {
-            let minarr = []
-            for(let t = 0;t<game.length;t++){
-                minarr.push(game[t].pair[1])
-            }
-            if(Math.max(...minarr) == -1){
-                ws.pair[1] = 0
-            }else{
-                if(!minarr.includes(7)){
-                    ws.pair[1] = 7
-                }
-                if(!minarr.includes(6)){
-                    ws.pair[1] = 6
-                }
-                if(!minarr.includes(5)){
-                    ws.pair[1] = 5
-                }
-                if(!minarr.includes(4)){
-                    ws.pair[1] = 4
-                }
-                if(!minarr.includes(3)){
-                    ws.pair[1] = 3
-                }
-                if(!minarr.includes(2)){
-                    ws.pair[1] = 2
-                }
-                if(!minarr.includes(1)){
-                    ws.pair[1] = 1
-                }
-                if(!minarr.includes(0)){
-                    ws.pair[1] = 0
-                }
-            }
-            let sjon = {
-                "index": `${game.indexOf(ws)}`,
-                "length": `${game.length}`,
-                "slot": `${ws.pair[1]}`
-            }
-            // sjon.minarr = minarr
-            let storage = []
-            for(let t = 0;t<game.length;t++){
-                if(t != game.indexOf(ws)){
-                    storage.push(game[t].storage)
-                }
-            }
-            sjon.storage = storage
-            ws.storage = storage
-            ws.serverID = data
-            ws.publicID = data
-            let ids = []
-            for(let t = 0;t<game.length;t++){
-                if(t != game.indexOf(ws)){
-                    ids.push(game[t].serverID)
-                }
-            }
-            sjon.playerIDs = ids
-            ws.send(JSON.stringify(sjon))
-        } else {
-            data = JSON.parse(data)
-            data.players = wss.clients.size
-            data.usedslots = []
 
-            let minarr = []
-            for(let t = 0;t<game.length;t++){
-                minarr.push(game[t].pair[1])
+        if(JSON.parse(data).kill == 1){
+            let sjon = {
+                "kill": `1`
             }
-            if(Math.max(...minarr) == -1){
-                ws.pair[1] = 0
-            }else{
-                if(!minarr.includes(7)){
-                    ws.pair[1] = 7
-                }
-                if(!minarr.includes(6)){
-                    ws.pair[1] = 6
-                }
-                if(!minarr.includes(5)){
-                    ws.pair[1] = 5
-                }
-                if(!minarr.includes(4)){
-                    ws.pair[1] = 4
-                }
-                if(!minarr.includes(3)){
-                    ws.pair[1] = 3
-                }
-                if(!minarr.includes(2)){
-                    ws.pair[1] = 2
-                }
-                if(!minarr.includes(1)){
-                    ws.pair[1] = 1
-                }
-                if(!minarr.includes(0)){
-                    ws.pair[1] = 0
-                }
-            }
-            // data.minarr = minarr
-            let ids = []
-            for(let t = 0;t<game.length;t++){
-                if(t != game.indexOf(ws)){
-                    ids.push(parseFloat(game[t].serverID))
-                }
-            }
-            data.playerIDs = ids
-            data = JSON.stringify(data)
             for (let t = 0; t < game.length; t++) {
-                if (ws != game[t]) {
-                    data = JSON.parse(data)
-                    data.serverID = ws.serverID
-                    data = JSON.stringify(data)
-                    game[t].send(data)
-                }else{
-                    game[t].storage = JSON.parse(data)
-                    game[t].serverID = JSON.parse(data).serverID
+                if (JSON.parse(data).striker == game[t].serverID) {
+                    game[t].send(JSON.stringify(sjon))
+                }
+            }
+
+        }else {
+            if (data >= 0) {
+                let minarr = []
+                for (let t = 0; t < game.length; t++) {
+                    minarr.push(game[t].pair[1])
+                }
+                if (Math.max(...minarr) == -1) {
+                    ws.pair[1] = 0
+                } else {
+                    if (!minarr.includes(7)) {
+                        ws.pair[1] = 7
+                    }
+                    if (!minarr.includes(6)) {
+                        ws.pair[1] = 6
+                    }
+                    if (!minarr.includes(5)) {
+                        ws.pair[1] = 5
+                    }
+                    if (!minarr.includes(4)) {
+                        ws.pair[1] = 4
+                    }
+                    if (!minarr.includes(3)) {
+                        ws.pair[1] = 3
+                    }
+                    if (!minarr.includes(2)) {
+                        ws.pair[1] = 2
+                    }
+                    if (!minarr.includes(1)) {
+                        ws.pair[1] = 1
+                    }
+                    if (!minarr.includes(0)) {
+                        ws.pair[1] = 0
+                    }
+                }
+                let sjon = {
+                    "index": `${game.indexOf(ws)}`,
+                    "length": `${game.length}`,
+                    "slot": `${ws.pair[1]}`
+                }
+                // sjon.minarr = minarr
+                let storage = []
+                for (let t = 0; t < game.length; t++) {
+                    if (t != game.indexOf(ws)) {
+                        storage.push(game[t].storage)
+                    }
+                }
+                sjon.storage = storage
+                ws.storage = storage
+                ws.serverID = data
+                ws.publicID = data
+                let ids = []
+                for (let t = 0; t < game.length; t++) {
+                    if (t != game.indexOf(ws)) {
+                        ids.push(game[t].serverID)
+                    }
+                }
+                sjon.playerIDs = ids
+                ws.send(JSON.stringify(sjon))
+            } else {
+                data = JSON.parse(data)
+                data.players = wss.clients.size
+                data.usedslots = []
+
+                let minarr = []
+                for (let t = 0; t < game.length; t++) {
+                    minarr.push(game[t].pair[1])
+                }
+                if (Math.max(...minarr) == -1) {
+                    ws.pair[1] = 0
+                } else {
+                    if (!minarr.includes(7)) {
+                        ws.pair[1] = 7
+                    }
+                    if (!minarr.includes(6)) {
+                        ws.pair[1] = 6
+                    }
+                    if (!minarr.includes(5)) {
+                        ws.pair[1] = 5
+                    }
+                    if (!minarr.includes(4)) {
+                        ws.pair[1] = 4
+                    }
+                    if (!minarr.includes(3)) {
+                        ws.pair[1] = 3
+                    }
+                    if (!minarr.includes(2)) {
+                        ws.pair[1] = 2
+                    }
+                    if (!minarr.includes(1)) {
+                        ws.pair[1] = 1
+                    }
+                    if (!minarr.includes(0)) {
+                        ws.pair[1] = 0
+                    }
+                }
+                // data.minarr = minarr
+                let ids = []
+                for (let t = 0; t < game.length; t++) {
+                    if (t != game.indexOf(ws)) {
+                        ids.push(parseFloat(game[t].serverID))
+                    }
+                }
+                data.playerIDs = ids
+                data = JSON.stringify(data)
+                for (let t = 0; t < game.length; t++) {
+                    if (ws != game[t]) {
+                        data = JSON.parse(data)
+                        data.serverID = ws.serverID
+                        data = JSON.stringify(data)
+                        game[t].send(data)
+                    } else {
+                        game[t].storage = JSON.parse(data)
+                        game[t].serverID = JSON.parse(data).serverID
+                    }
                 }
             }
         }
